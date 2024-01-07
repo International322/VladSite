fcd528d8f471b64a7a4077629645604ff259e40e
Мак <mak@MacBook-Pro-Mak.local>
2024-01-07 17:25:18 +0500

    Ваш комментарий к коммиту

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -3,25 +3,25 @@ import styles from './Header.module.scss';
 import { MENU } from './header.data';
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.logo_container}>
-          <Link className={styles.logo} href="/">
-            Логотип
-          </Link>
-        </div>
-        <ul className="flex gap-8">
-          {MENU.map(item => (
-            <li className={styles.menuItem} key={item.url}>
-              <Link href={item.url}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-        <button className={styles.button}>Связаться по телефону</button>
-      </div>
-    </header>
-  );
+	return (
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<div className={styles.logo_container}>
+					<Link className={styles.logo} href="/">
+						Логотип
+					</Link>
+				</div>
+				<ul className={styles.menuList}>
+					{MENU.map((item) => (
+						<li className={styles.menuItem} key={item.url}>
+							<Link href={item.url}>{item.name}</Link>
+						</li>
+					))}
+				</ul>
+				<button className={styles.button}>Связаться по телефону</button>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
