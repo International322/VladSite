d13557d842a5fdbfd392dba7db97d222e3e224fa
IvRyzh <322qwertyuiop@gmail.com>
2024-01-07 23:34:07 +0500

    Ваш комментарий к коммиту

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+        return {
+            '/': { page: '/' },
+            // Добавьте другие маршруты, если они есть в вашем проекте
+        };
+    },
 }
 
 module.exports = nextConfig
