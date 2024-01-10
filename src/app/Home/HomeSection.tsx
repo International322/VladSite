b81e1824847ad63891229f3870ec694c5cfc0ee3
Мак <mak@MacBook-Pro-Mak.local>
2024-01-10 21:47:32 +0500

    a

--- src/app/Home/HomeSection.tsx
+++ src/app/Home/HomeSection.tsx
@@ -1,24 +1,24 @@
 import styles from './Home.module.scss';
 
 export interface HomeSectionProps {
-  children: React.ReactNode;
-  id: number;
-  src: string;
-  className?: string;
+	children: React.ReactNode;
+	id: number;
+	src: string;
+	className?: string;
 }
 
 const HomeSection: React.FC<HomeSectionProps> = ({
-  children,
-  id,
-  src,
-  className,
-  ...props
+	children,
+	id,
+	src,
+	className,
+	...props
 }) => {
-  return (
-    <section className={className}>
-      <div className={styles.container}>{children}</div>
-    </section>
-  );
+	return (
+		<section className={className}>
+			<div className={styles.container}>{children}</div>
+		</section>
+	);
 };
 
 export default HomeSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,46 +4,45 @@ import styles from './Home.module.scss';
 import CircularProgressBar from '../components/ui/CircularProgressBar/CircularProgressBar';
 
 export default function Home() {
-  const progress = 55;
+	const progress = 55;
 
-  const firstSection = sectionData[0];
+	const firstSection = sectionData[0];
 
-  return (
-    <div className="">
-      <HomeSection
-        id={firstSection.id}
-        src={firstSection.body__image}
-        className="bg-build1"
-      >
-        <div className={styles.header__content1}>
-          <h1 className={styles.header__title1}>
-            {firstSection.header__title}
-          </h1>
-          <p className={styles.header__text1}>{firstSection.header__text}</p>
-          <button className={styles.header__button1}>
-            {firstSection.header__button}
-          </button>
-        </div>
-        <div className={styles.body__content1}>
-          <img src={firstSection.body__image} alt="image" />
-          <p className={styles.body__text1}>{firstSection.body__text}</p>
-        </div>
-      </HomeSection>
-      <HomeSection id={2} src="" className="bg-brown-100">
-        <div>
-          <h1>О НАС В ЦИФРАХ</h1>
-        </div>
-        <div className="">
-          <div className="flex">
-            <CircularProgressBar progress={progress} />
-            <CircularProgressBar progress={progress} />
-            <CircularProgressBar progress={progress} />
-          </div>
-          <p>
-            Бесплатные консультации по проекту в течении срока строительства
-          </p>
-        </div>
-      </HomeSection>
-    </div>
-  );
+	return (
+		<div className="">
+			<HomeSection
+				id={firstSection.id}
+				src={firstSection.body__image}
+				className={`${styles.section__1}`}
+			>
+				<div className={styles.header__content1}>
+					<h1 className={styles.header__title1}>
+						{firstSection.header__title}
+					</h1>
+					<p className={styles.header__text1}>{firstSection.header__text}</p>
+					<button className={styles.header__button1}>
+						{firstSection.header__button}
+					</button>
+				</div>
+				<div className={styles.body__content1}>
+					<img className="" src={firstSection.body__image} alt="image" />
+				</div>
+			</HomeSection>
+			<HomeSection id={2} src="" className="bg-brown-100">
+				<div>
+					<h1>О НАС В ЦИФРАХ</h1>
+				</div>
+				<div className="">
+					<div className="flex">
+						<CircularProgressBar progress={progress} />
+						<CircularProgressBar progress={progress} />
+						<CircularProgressBar progress={progress} />
+					</div>
+					<p>
+						Бесплатные консультации по проекту в течении срока строительства
+					</p>
+				</div>
+			</HomeSection>
+		</div>
+	);
 }
